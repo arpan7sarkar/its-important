b1fcf8748849db69afb7e66e19afdff9339d3183
arpan7sarkar <arpansakar@gmail.com>
2026-01-11 18:11:24 +0530

    defined models and wrote the starting point of the express server

---
+++ src/model/ContentModel.ts
@@ -0,0 +1,36 @@
+import mongoose, { type ObjectId } from "mongoose";
+import { User } from "./UserModel.js";
+
+const contentTypes = ["image", "video", "article", "audio", "shorts", "others"];
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const ContentSchema = new mongoose.Schema({
+  link: { type: String, required: true },
+  type: { type: String, enum: contentTypes, required: true },
+  title: { type: String, required: true },
+  tags: [{ type: ObjectId, ref: "Tag" }],
+  userId: { type: ObjectId, ref: "User", required: true },
+   validate: async (value: ObjectId)=> {
+      const user = await User.findById(value);
+      if (!user) {
+        throw new Error('User does not exist');
+      }
+    }
+});
+
+const LinkSchema = new mongoose.Schema({
+  hash: { type: String, required: true },
+  userId: { type: ObjectId, ref: "User", required: true },
+  validate: async (value: ObjectId)=> {
+      const user = await User.findById(value);
+      if (!user) {
+        throw new Error('User does not exist');
+      }
+    }
+});
+
+const Content = mongoose.model("Content", ContentSchema);
+const Link = mongoose.model("Link", LinkSchema);
+
+export default { Content, Link };
---
+++ src/model/UserModel.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User= mongoose.model( "User", UserSchema)
+
+export {User};
